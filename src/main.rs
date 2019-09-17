98bdaac85e7fc7b155ba9668037759946835c0df
Stewart Wilson <thekidxp@gmail.com>
2019-09-16 22:38:55 -0400

    Update some of the templating

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use serde_json::json;
+use std::io;
+use handlebars::Handlebars;
+
 
 #[get("/")]
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+fn index(hb: web::Data<Handlebars>) -> HttpResponse {
+    let data = json!({
+        "motivation": "hello"
+    });
+    let body = hb.render("motivation", &data).unwrap();
+
+    HttpResponse::Ok().body(body)
 }
 
-fn main() {
-    HttpServer::new(|| {
+
+fn main() -> io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./templates")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
+    HttpServer::new(move || {
         App::new()
+            .register_data(handlebars_ref.clone())
             .service(index)
     })
         .bind("127.0.0.1:8088")
         .unwrap()
         .run()
-        .unwrap();
 }
