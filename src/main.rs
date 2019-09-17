b12726111be3a6c91bcdb4b23271836de2e11f73
Stewart Wilson <thekidxp@gmail.com>
2019-09-16 20:51:33 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn main() {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run()
+        .unwrap();
+}
