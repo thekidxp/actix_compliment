73c7e8b91eba036ba762fb4493959245485b60b9
swilson2 <swilson2@vitamix.com>
2019-09-17 12:14:47 -0400

    Finish template and use Rand
    Now actually using the rand crate to select all random elements of the
    template.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use actix_files as fs;
+use actix_web::{get, web, App, HttpResponse, HttpServer};
+use handlebars::Handlebars;
+use motivations::MOTIVATIONS;
+use rand::prelude::SliceRandom;
+use rand::thread_rng;
 use serde_json::json;
 use std::io;
-use handlebars::Handlebars;
-
 
 #[get("/")]
 fn index(hb: web::Data<Handlebars>) -> HttpResponse {
+    let mut rng = thread_rng();
+    let &crab = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+    ]
+    .choose(&mut rng)
+    .unwrap();
+    let &motivation = MOTIVATIONS.choose(&mut rng).unwrap();
     let data = json!({
-        "motivation": "hello"
+        "motivation": motivation,
+        "image": crab
     });
     let body = hb.render("motivation", &data).unwrap();
 
     HttpResponse::Ok().body(body)
 }
-
 
 fn main() -> io::Result<()> {
     let mut handlebars = Handlebars::new();
@@ -26,8 +36,8 @@ fn main() -> io::Result<()> {
         App::new()
             .register_data(handlebars_ref.clone())
             .service(index)
+            .service(fs::Files::new("/public", "public/"))
     })
-        .bind("127.0.0.1:8088")
-        .unwrap()
-        .run()
+    .bind("127.0.0.1:8088")?
+    .run()
 }
